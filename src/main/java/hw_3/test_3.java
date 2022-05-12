bc5845298e70d86d8e19e5e0e5ce09ae28a7ee78
TadeyshD <86590703+TadeyshD@users.noreply.github.com>
2022-05-13 01:00:00 +0300

    Hlp

--- src/main/java/hw_3/test_3.java
+++ src/main/java/hw_3/test_3.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -33,35 +34,23 @@ public class test_3 {
         action.switchTo().frame(0);
         WebElement webElement4 = action.findElement(By.xpath(".//*[@accesskey=\"t\"]"));
         webElement4.click();
-        action.switchTo().parentFrame();
-        action.switchTo().frame(1);
-        action.switchTo().frame(0);
+        action.switchTo().parentFrame().switchTo().frame(1).switchTo().frame(0);
         WebElement webElement5 = action.findElement(By.cssSelector(".x-tree-ec-icon"));
         webElement5.click();
-        WebElement webElement6 = action.findElement(By.cssSelector("#extdd-13 > .x-tree-ec-icon"));
+        WebElement webElement6 = action.findElement(By.cssSelector("#extdd-10 > .x-tree-ec-icon"));
         webElement6.click();
-        action.switchTo().parentFrame();
-        action.switchTo().frame(0);
-        WebElement webElement7 = action.findElement(By.id("extdd-861"));
+        WebElement webElement7 = action.findElement(By.cssSelector("#extdd-864"));
         webElement7.click();
-        WebElement webElement8 = action.findElement(By.cssSelector("div > input:nth-child(1)"));
+        action.switchTo().parentFrame();
+        action.switchTo().frame(1);
+        WebElement webElement8 = action.findElement(By.xpath("//form[@id='stepsControls']/div/div[4]/input"));
         webElement8.click();
-        action.switchTo().frame(0);
-        WebElement webElement9 = action.findElement(By.cssSelector(".cke_editable"));
-        webElement9.click();
-        webElement9.sendKeys("<p>Новый шаг для меня, не для человечества, просьба не путать</p>");
-        action.switchTo().parentFrame();
-        action.switchTo().frame(0);
-        WebElement webElement10 = action.findElement(By.cssSelector(".x-tree-elbow-minus"));
-        webElement10.click();
-        WebElement webElement11 = action.findElement(By.cssSelector(".x-tree-elbow-end-minus"));
-        webElement11.click();
-        action.switchTo().parentFrame();
-        action.switchTo().parentFrame();
-        action.switchTo().frame(0);
-        WebElement webElement12 = action.findElement(By.xpath(".//*[@accesskey=\"q\"]"));
-        webElement12.click();
-        action.quit();
+        try {
+            action.switchTo().frame(0);
+        } catch (StaleElementReferenceException e) {
+            System.out.println(e.getSupportUrl());
+        }
+
     }
 }
 
