afdd970e78f05b7db751fb7bf71f5b20e75983db
TadeyshD <verydeadman03@gmail.com>
2022-05-13 11:34:56 +0300

    finally

--- src/main/java/hw_3/test_3.java
+++ src/main/java/hw_3/test_3.java
@@ -8,6 +8,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -41,16 +44,28 @@ public class test_3 {
         webElement6.click();
         WebElement webElement7 = action.findElement(By.cssSelector("#extdd-864"));
         webElement7.click();
-        action.switchTo().parentFrame();
-        action.switchTo().frame(1);
-        WebElement webElement8 = action.findElement(By.xpath("//form[@id='stepsControls']/div/div[4]/input"));
+        action.switchTo().parentFrame().switchTo().frame(1);
+        WebElement webElement8 = action.findElement(By.xpath(".//div[4]/input"));
         webElement8.click();
         try {
-            action.switchTo().frame(0);
-        } catch (StaleElementReferenceException e) {
-            System.out.println(e.getSupportUrl());
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-
+        action.switchTo().frame(0);
+        WebElement webElement9 = action.findElement(By.cssSelector(".cke_editable"));
+        webElement9.click();
+        webElement9.sendKeys("Привет");
+        action.switchTo().parentFrame();
+        WebElement webElement10 = action.findElement(By.xpath("//div[5]/div/input[2]"));
+        webElement10.click();
+        action.switchTo().parentFrame().switchTo().frame(0);
+        WebElement webElement11 = action.findElement(By.cssSelector(".x-tree-elbow-minus"));
+        webElement11.click();
+        action.switchTo().parentFrame().switchTo().parentFrame().switchTo().frame(0);
+        WebElement webElement12 = action.findElement(By.xpath(".//*[@accesskey=\"q\"]"));
+        webElement12.click();
+        action.quit();
     }
 }
 
--- src/main/java/hw_3/test_4.java
+++ src/main/java/hw_3/test_4.java
@@ -2,10 +2,15 @@ package hw_3;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -32,35 +37,41 @@ public class test_4 {
         action.switchTo().frame(0);
         WebElement webElement4 = action.findElement(By.xpath(".//*[@accesskey=\"t\"]"));
         webElement4.click();
-        action.switchTo().parentFrame();
-        action.switchTo().frame(1);
-        action.switchTo().frame(0);
+        action.switchTo().parentFrame().switchTo().frame(1).switchTo().frame(0);
         WebElement webElement5 = action.findElement(By.cssSelector(".x-tree-ec-icon"));
         webElement5.click();
-        WebElement webElement6 = action.findElement(By.cssSelector("css=#extdd-19 > .x-tree-ec-icon"));
+        WebElement webElement6 = action.findElement(By.cssSelector("#extdd-10 > .x-tree-ec-icon"));
         webElement6.click();
-        WebElement webElement7 = action.findElement(By.id("extdd-840"));
+        WebElement webElement7 = action.findElement(By.cssSelector("#extdd-864"));
         webElement7.click();
-        action.switchTo().parentFrame();
-        action.switchTo().frame(1);
-        WebElement webElement8 = action.findElement(By.name("create_step"));
+        action.switchTo().parentFrame().switchTo().frame(1);
+        WebElement webElement8 = action.findElement(By.xpath(".//div[4]/input"));
         webElement8.click();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         action.switchTo().frame(0);
         WebElement webElement9 = action.findElement(By.cssSelector(".cke_editable"));
         webElement9.click();
-        webElement9.sendKeys("<p>Oh god, you from england</p>");
+        webElement9.sendKeys("Привет");
         action.switchTo().parentFrame();
         WebElement webElement10 = action.findElement(By.cssSelector(".workBack:nth-child(16) #do_update_step_and_exit"));
         webElement10.click();
-        WebElement webElement11 = action.findElement(By.cssSelector("#step_row_41577 > .clickable_icon:nth-child(5) > img"));
+        WebElement webElement11 = action.findElement(By.xpath("(//img[@alt='Удалить шаг'])[19]"));
         webElement11.click();
-        WebElement webElement12 = action.findElement(By.id("ext-gen20"));
+        WebElement webElement12 = action.findElement(By.cssSelector("#ext-comp-1002 .x-btn-mc"));
         webElement12.click();
-        action.switchTo().parentFrame();
-        action.switchTo().parentFrame();
-        action.switchTo().frame(0);
-        WebElement webElement13 = action.findElement(By.xpath(".//*[@accesskey=\"q\"]"));
+        action.switchTo().parentFrame().switchTo().frame(0);
+        WebElement webElement13 = action.findElement(By.cssSelector(".x-tree-elbow-minus"));
         webElement13.click();
+        WebElement webElement14 = action.findElement(By.cssSelector(".x-tree-elbow-end-minus"));
+        webElement14.click();
+        action.switchTo().parentFrame().switchTo().parentFrame().switchTo().frame(0);
+        WebElement webElement15 = action.findElement(By.xpath(".//*[@accesskey=\"q\"]"));
+        webElement15.click();
         action.quit();
     }
 }
+
